a8017158b6f059cbf4952fae15c59dc1b00a09cd
sheetalshinde8421-afk <sheetalshinde8421@gmail.com>
2026-09-18 15:48:26 +0530

    Create calculators.py

---
+++ calculators.py
@@ -0,0 +1,7 @@
+# Calculator - Addition and Subtraction
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
